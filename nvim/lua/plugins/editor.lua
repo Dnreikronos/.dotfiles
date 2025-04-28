69adddb5bbc393ded994cfb5ccae933ab2e2917f
Dnreikronos <dnreikronos2@gmail.com>
2025-04-28 18:48:02 -0300

    refactor: change theme to tokyo night

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -110,70 +110,14 @@ return {
 			{ "<C-l>",     function() require("harpoon.ui").nav_file(4) end,         desc = "Go to file 4" }
 		}
 	},
-
-	-- ✅ Theme: Black and Green
 	{
-		"Mofiqul/vscode.nvim",
+		"folke/tokyonight.nvim",
 		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.o.background = "dark"
-
-			require("vscode").setup({
-				transparent = false,
-				italic_comments = true,
-				disable_nvimtree_bg = true,
-				color_overrides = {
-					vscBack = "#000000",
-					vscLineNumber = "#00ff00",
-				},
-				group_overrides = {
-					Normal = { fg = "#00ff00", bg = "#000000" },
-					NormalNC = { fg = "#00ff00", bg = "#000000" },
-					LineNr = { fg = "#00ff00" },
-					CursorLineNr = { fg = "#00ff00", bold = true },
-					Comment = { fg = "#00ff00", italic = true },
-					Function = { fg = "#00ff00" },
-					Identifier = { fg = "#00ff00" },
-					Statement = { fg = "#00ff00", bold = true },
-					Type = { fg = "#00ff00" },
-					String = { fg = "#00ff00" },
-					Keyword = { fg = "#00ff00", bold = true },
-					Constant = { fg = "#00ff00" },
-					Operator = { fg = "#00ff00" },
-					PreProc = { fg = "#00ff00" },
-					Special = { fg = "#00ff00" },
-					Todo = { fg = "#000000", bg = "#00ff00", bold = true },
-				}
-			})
-
-			require("vscode").load()
+			vim.cmd[[colorscheme tokyonight]]
 		end
 	},
-
-	-- Disabled: OneDark
-	-- {
-	-- 	"navarasu/onedark.nvim",
-	-- 	lazy = false,
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		require("onedark").setup({
-	-- 			style = "deep",
-	-- 			transparent = true,
-	-- 			term_colors = true,
-	-- 			ending_tildes = false,
-	-- 			code_style = {
-	-- 				comments = "italic",
-	-- 				keywords = "bold",
-	-- 				functions = "italic",
-	-- 				strings = "none",
-	-- 				variables = "none"
-	-- 			}
-	-- 		})
-	-- 		require("onedark").load()
-	-- 	end
-	-- },
-
 	{
 		"mbbill/undotree",
 		keys = {
