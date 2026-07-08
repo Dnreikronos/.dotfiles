aa6d82f2b41628ad0afc687072fd3f3e0c769eea
Dnreikronos <dnreikronos2@gmail.com>
2026-07-08 16:38:24 -0300

    Disable Neovim indent guides

--- nvim-windows/nvim/lua/autocmds.lua
+++ nvim-windows/nvim/lua/autocmds.lua
@@ -8,3 +8,30 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     vim.bo.commentstring = "# %s"
   end,
 })
+
+local no_indent_guides = vim.api.nvim_create_augroup("NoIndentGuides", { clear = true })
+
+local function clear_indent_guides()
+  pcall(vim.cmd, "IBLDisable")
+
+  for _, group in ipairs({
+    "IblChar",
+    "IblScopeChar",
+    "@ibl.scope.underline.1",
+    "@ibl.scope.underline.2",
+    "@ibl.scope.underline.3",
+    "@ibl.scope.underline.4",
+    "@ibl.scope.underline.5",
+    "@ibl.scope.underline.6",
+    "@ibl.scope.underline.7",
+  }) do
+    vim.api.nvim_set_hl(0, group, { fg = "NONE", bg = "NONE", sp = "NONE", underline = false })
+  end
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter", "BufEnter", "ColorScheme" }, {
+  group = no_indent_guides,
+  callback = clear_indent_guides,
+})
+
+clear_indent_guides()
